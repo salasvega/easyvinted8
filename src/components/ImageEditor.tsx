106172f303cdf98dc2237a1481088f896806bd40
salasvega <salasvega@gmail.com>
2026-02-13 18:08:36 +0100

    Updated ImageEditor.tsx

--- src/components/ImageEditor.tsx
+++ src/components/ImageEditor.tsx
@@ -655,7 +655,7 @@ export function ImageEditor({
                         ⚠️ Les rendus sont volontairement naturels (style “photo iPhone”) car Vinted privilégie les visuels authentiques.
                         <br />
                         <br />
-                       Vous êtes trop moche ou fatigué de poser? Utilisez un Modèle (Avatar) pour porter l’article:
+                       Vous êtes trop moche ou fatigué de poser? Utilisez un Modèle (Avatar) pour porter l’article à cotre place :)
 Sélectionnez-le, choisissez l’option “Porté” et laissez la magie opérer. Le visage sera automatiquement masqué pour rester discret.
                         <br />
                         <br />
