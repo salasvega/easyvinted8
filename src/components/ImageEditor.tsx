3ee44512fc703d5c1b11a1d1e566a89a313664bf
salasvega <salasvega@gmail.com>
2026-02-13 18:01:36 +0100

    Updated ImageEditor.tsx

--- src/components/ImageEditor.tsx
+++ src/components/ImageEditor.tsx
@@ -649,7 +649,7 @@ export function ImageEditor({
                         Studio Magik-AI
                       </h3>
                       <p className="text-slate-700 text-sm leading-relaxed">
-                        Décrivez les modifications que vous souhaitez apporter pour mettre en valeur votre article et laissez Studio Magik-AI les réaliser.
+                        Choisissez une option, une pose ou décrivez les modifications que vous souhaitez apporter à la aphoto séléctionnée pour mettre en valeur votre article. Cliquez sur Editer et laissez le Studio Magik-AI les réaliser!
                         <br />
                         <br />
                         ⚠️ Les rendus générés ont volontairement un style naturel, "vrai iPhone / pas pro", car Vinted n'aime pas les photos trop parfaites. On pense à tout :)
