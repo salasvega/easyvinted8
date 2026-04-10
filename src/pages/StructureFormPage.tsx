d91c6291c4373016c782368e5440557fb9451384
salasvega <salasvega@gmail.com>
2026-04-10 21:43:04 +0200

    Adjust header spacing

--- src/pages/StructureFormPage.tsx
+++ src/pages/StructureFormPage.tsx
@@ -179,7 +179,7 @@ export function StructureFormPage() {
   ];
 
   return (
-    <div className="max-w-4xl mx-auto px-4 py-8">
+    <div className="max-w-4xl mx-auto px-4 pt-4 pb-8">
       {toast && (
         <Toast
           message={toast.message}
